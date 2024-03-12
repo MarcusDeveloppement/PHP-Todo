c77d6da8dbba69b121776dd847f8e74c83848dfb
Marcus <marcus.frontdev@gmail.com>
2024-03-12 16:10:02 +0100

    begins to make style for this todo

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script defer src="public/js/index.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <title>Todo</title>
+</head>
+<body>
+    <div class="container">
+    <header>
+        <div class="logo">ToDo List</div>
+    </header>
+    <div class="content">
+        <div class="todo-container">
+            <h1>Todo</h1>
+            <div class="todo-form"></div>
+            <div class="todo-list"></div>
+        </div>
+    </div>
+    <footer><p>2024 © Tous droits réservés</p></footer>
+    </div>
+</body>
+</html>
